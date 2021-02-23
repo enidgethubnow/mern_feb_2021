571b217a0a98ae9f321ebf8916bdc2f30ed5134c
Kevin Udink <kudink@gmail.com>
2021-02-23 16:14:09 -0700

    Updated Version of w3d1 to use: objects for Errors and input element state Added a lot of logic using new concepts Switch Statements Delete Keys from objects How to determine if an object is empty Helper functions to accomplish tasks using input element name as keys to update objects

--- w3d1/src/App.js
+++ w3d1/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import WorkExperience from './components/WorkExperience';
+// import WorkExperiencesStateObj from './components/WorkExperienceStateObj';
 
 function App() {
 
   return (
     <div className="App">
+      {/* <WorkExperiencesStateObj /> */}
+      {/* <hr /> */}
       <WorkExperience />
     </div>
   );
