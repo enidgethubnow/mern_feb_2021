0cc736957d016f03d7288905b305310d9a5aba2e
Kevin Udink <kudink@gmail.com>
2021-02-24 13:31:26 -0700

    w3d2 start

---
+++ w3d2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import WorkExperience from './components/WorkExperience';
+// import WorkExperiencesStateObj from './components/WorkExperienceStateObj';
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <WorkExperiencesStateObj /> */}
+      {/* <hr /> */}
+      <WorkExperience />
+    </div>
+  );
+}
+
+export default App;
